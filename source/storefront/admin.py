fda3e5440934c80ea2e67a1a290507d6fb9a1c0a
Said <s.avkhadeev@gmail.com>
2021-10-28 23:09:10 +0600

    Created models for storefront app

--- source/storefront/admin.py
+++ source/storefront/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from storefront.models import Product, Category, Brand, Stock, ImageContainer, Image
 
-# Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Stock)
+admin.site.register(ImageContainer)
+admin.site.register(Image)
