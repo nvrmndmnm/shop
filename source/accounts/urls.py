eb90a1aa5e0fbf6c4482c224d367e1ed64251631
Said <s.avkhadeev@gmail.com>
2021-10-30 14:51:50 +0600

    Added user accounts

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from accounts.views import UserListView
+from accounts.views import UserCreateView, UserDetailView, UserUpdateView, UserPasswordChangeView
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', UserListView.as_view(), name='profiles')
+    path('signup/', UserCreateView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('<int:pk>/', UserDetailView.as_view(), name='profile'),
+    path('update/', UserUpdateView.as_view(), name='update'),
+    path('change-password/', UserPasswordChangeView.as_view(), name='update_password')
 ]
