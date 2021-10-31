b79ca68edb786d6841134d701bd092104c161778
Said <s.avkhadeev@gmail.com>
2021-10-31 21:36:54 +0600

    Added new storefront templates, changed views

--- source/storefront/admin.py
+++ source/storefront/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from storefront.models import Product, Category, Brand, Stock, ImageContainer, Image
+from storefront.models import Product, Category, Brand, Stock, PriceList, ImageContainer, Image
 
 
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Brand)
 admin.site.register(Stock)
+admin.site.register(PriceList)
 admin.site.register(ImageContainer)
 admin.site.register(Image)
--- source/storefront/urls.py
+++ source/storefront/urls.py
@@ -5,4 +5,7 @@ app_name = 'storefront'
 
 urlpatterns = [
     path('', storefront_views.IndexView.as_view(), name='index'),
+    path('p/<int:pk>/', storefront_views.ProductDetailView.as_view(), name='product'),
+    path('c/<int:pk>/', storefront_views.ProductListView.as_view(), name='category'),
+    path('search/', storefront_views.ProductSearchView.as_view(), name='search')
 ]
