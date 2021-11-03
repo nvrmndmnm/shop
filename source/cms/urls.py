dd486aaa176e6ba7b271bcca72bd4c6aeacd5e67
Said <s.avkhadeev@gmail.com>
2021-11-03 14:56:08 +0600

    implement orders processing in CMS

--- source/cms/urls.py
+++ source/cms/urls.py
@@ -4,7 +4,12 @@ from cms import views as cms_views
 app_name = 'cms'
 
 urlpatterns = [
-    path('', cms_views.ProductListView.as_view(), name='cms'),
+    path('', cms_views.NewOrderListView.as_view(), name='cms'),
+    path('orders/new/', cms_views.NewOrderListView.as_view(), name='orders_new'),
+    path('orders/processed/', cms_views.ProcessedOrderListView.as_view(), name='orders_processed'),
+    path('orders/archive/', cms_views.ArchivedOrderListView.as_view(), name='orders_archived'),
+    path('orders/<int:pk>', cms_views.OrderDetailView.as_view(), name='order'),
+    path('orders/<int:pk>/process/', cms_views.process_order, name='process_order'),
     path('products/', cms_views.ProductListView.as_view(), name='products'),
     path('products/create/', cms_views.ProductCreateView.as_view(), name='product_create'),
     path('products/<int:pk>/', cms_views.ProductUpdateView.as_view(), name='product_update'),
---
+++ source/cms/views/order_views.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import ListView, DetailView
+
+from cart.models import Order
+
+
+class NewOrderListView(PermissionRequiredMixin, ListView):
+    model = Order
+    template_name = 'orders/list.html'
+    paginate_by = 10
+    ordering = ['time_created']
+    permission_required = 'employee'
+    login_url = 'accounts:login'
+
+    def get_queryset(self):
+        return Order.objects.filter(status='PROCESSING')
+
+
+class ProcessedOrderListView(NewOrderListView):
+    def get_queryset(self):
+        return Order.objects.filter(status='ON_DELIVERY')
+
+
+class ArchivedOrderListView(NewOrderListView):
+    def get_queryset(self):
+        return Order.objects.filter(status__in=['CANCELED', 'RETURNED', 'FINISHED'])
+
+
+class OrderDetailView(PermissionRequiredMixin, DetailView):
+    model = Order
+    template_name = 'orders/detail.html'
+    permission_required = 'employee'
+    login_url = 'accounts:login'
+
+
+@permission_required(perm='employee', login_url='accounts:login')
+def process_order(request, **kwargs):
+    order = get_object_or_404(Order, pk=kwargs.get("pk"))
+    order.status = 'ON_DELIVERY'
+    order.save()
+    return redirect('cms:orders_new')
